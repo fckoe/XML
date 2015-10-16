c3b42c9a24d13d051f667c39e23b3fda1b8be2e1
Francisco Eris <fco.eris@gmail.com>
2015-10-16 20:15:27 -0300

    Se crea clase XML

---
+++ XML/cambiaAtributo/cambiaAtributo/Xml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cambiaAtributo
+{
+    class Xml
+    {
+        string variableName;
+        string variableValue;
+
+        /*
+         * CONSTRUCTOR 
+         */
+        public Xml()
+        {
+        }
+
+
+        /*
+         *  GETTER AND SETTER
+         */
+        public void setVariableName(string variableName)
+        {
+            this.variableName = variableName;
+        }
+
+        public void setVariableValue(string variableValue)
+        {
+            this.variableValue = variableValue;
+        }
+
+        public string getVariableName()
+        {
+            return this.variableName;
+        }
+
+        public string getVariableValue()
+        {
+            return this.variableValue;
+        }
+    }
+}
