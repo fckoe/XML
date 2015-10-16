342ebbdafe44a23dbaef70e514177b68fa501d42
Francisco Eris <fco.eris@gmail.com>
2015-10-16 20:17:53 -0300

    se crea clase de variable de entorno

---
+++ XML/cambiaAtributo/cambiaAtributo/VariableEntorno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cambiaAtributo
+{
+    class VariableEntorno
+    {
+        string variableName;
+        string variableValue;
+
+        /*
+            *  GETTER AND SETTER
+            */
+        public void setVariableName(string variableName)
+        {
+            this.variableName = variableName;
+        }
+
+        public void setVariableValue(string variableValue)
+        {
+            this.variableValue = variableValue;
+        }
+
+        public string getVariableName()
+        {
+            return this.variableName;
+        }
+
+        public string getVariableValue()
+        {
+            return this.variableValue;
+        }
+    }
+}
