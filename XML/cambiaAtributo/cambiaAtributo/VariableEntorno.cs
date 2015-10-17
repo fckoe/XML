53ccb5bc39861765877b86ce8d75a50989fbcaf7
Francisco Eris <fco.eris@gmail.com>
2015-10-16 21:20:43 -0300

    se quita getter and setter de la clase variableEntorno

--- XML/cambiaAtributo/cambiaAtributo/VariableEntorno.cs
+++ XML/cambiaAtributo/cambiaAtributo/VariableEntorno.cs
@@ -8,36 +8,7 @@ namespace cambiaAtributo
 {
    public  class VariableEntorno
     {
-        string variableName;
-        string variableValue;
 
-        /*
-         * CONTRUCTOR
-         */
-        public VariableEntorno() { }
-
-        /*
-            *  GETTER AND SETTER
-            */
-        public void setVariableName(string variableName)
-        {
-            this.variableName = variableName;
-        }
-
-        public void setVariableValue(string variableValue)
-        {
-            this.variableValue = variableValue;
-        }
-
-        public string getVariableName()
-        {
-            return this.variableName;
-        }
-
-        public string getVariableValue()
-        {
-            return this.variableValue;
-        }
 
         public Boolean existeVariable( string variableName) {
             if (Environment.GetEnvironmentVariable(variableName) == null)
