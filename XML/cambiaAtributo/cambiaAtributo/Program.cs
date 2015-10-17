85fceec27a48815395ab8f1e6ada309fc8f53bea
Francisco Eris <fco.eris@gmail.com>
2015-10-17 19:18:06 -0300

    se ingresan mensajes para saber saber que es lo que hace la aplicacion

--- XML/cambiaAtributo/cambiaAtributo/Program.cs
+++ XML/cambiaAtributo/cambiaAtributo/Program.cs
@@ -13,13 +13,13 @@ namespace cambiaAtributo
         static void Main(string[] args)
         {
 
+
+            Console.WriteLine("00. Inicializar Variables");
             string nombreVariable = ConfigurationManager.AppSettings["variableEntorno"].ToString();
             string rutaVariable = null;
             string valorWebService = null;
             string rutaInstalacion = null;
             string rutaNodo = null;
-            Console.WriteLine(nombreVariable);
-            Console.ReadLine();
             Xml xml = new Xml();
             XmlDocument xmlDoc = new XmlDocument();
             
@@ -28,20 +28,27 @@ namespace cambiaAtributo
             try
             {
                 rutaVariable = xml.obtieneValorVariable(nombreVariable);
+
+                Console.WriteLine("10. Validar Variable de Entorno ");
                 if (rutaVariable != null)
                 {
+                    Console.WriteLine("20. Obtener Valores de App.Config");
                     valorWebService = ConfigurationManager.AppSettings["nuevaRutaWebService"].ToString();
                     rutaInstalacion = ConfigurationManager.AppSettings["rutaInstalacion"].ToString();
                     rutaNodo = ConfigurationManager.AppSettings["rutaNodoWebService"].ToString();
 
                     xml.setRutaXml(rutaVariable + rutaInstalacion);
                     xml.setNombreAtributo(ConfigurationManager.AppSettings["nombreAtributo"].ToString());
+                    Console.WriteLine("30. Abrir XML de configuracion de impresora");
                     xmlDoc.Load(xml.getRutaXml());
 
-                    
+                    Console.WriteLine("40. Obtener nodo de XML de impresion");
                     XmlNode node = xmlDoc.SelectSingleNode(rutaNodo);
+                    Console.WriteLine("50. Cambiar valor del atributo "+xml.getNombreAtributo());
                     node.Attributes[xml.getNombreAtributo()].Value = valorWebService;
+                    Console.WriteLine("60. Guardar XML de configuración de impresora");
                     xmlDoc.Save(xml.getRutaXml());
+                    Console.WriteLine("70. Fin. ## PRESIONE UNA TECLA PARA CERRAR LA APLICACION ##");
                     Console.ReadLine();
    
                     
@@ -49,13 +56,14 @@ namespace cambiaAtributo
 
                 }
                 else {
-                    Console.WriteLine("Variable no tiene valor");
+                    Console.WriteLine("15. Variable de entorno "+nombreVariable+" no tiene valor. Presione una tecla para salir.");
+                    Console.ReadLine()
                 }
                 
             }
             catch (Exception e) {
-                Console.Clear();
-                Console.WriteLine("Error en la aplicación Contactese con casa central "+e.Message);
+                Console.WriteLine(" 18. Error en la aplicación Contactese con casa central "+e.Message);
+                Console.ReadLine()
 
             }
 
